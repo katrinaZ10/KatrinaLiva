9c2077afd340c2efc79e66c3a1f176d22f0ca2e4
katrinaZ10 <144447069+katrinaZ10@users.noreply.github.com>
2023-09-12 11:21:31 +0300

    Add files via upload

---
+++ Tests_liva_EvelinaMarta.py
@@ -0,0 +1,11 @@
+#30 dienas python programmēšanas valodā
+name = 'Evelina'
+surname = ' Zundmane'
+full_name = name+surname
+country, city = 'Australia', 'Kanbera'
+age = 18
+year = 2030
+
+print(full_name,'\n',country,'\n',city,'\n',age,'\n', year)
+
+print(type(name), '\n',type(surname), '\n',type(full_name), '\n',type(country), '\n',type(city), '\n',type(age), '\n',type(year))
