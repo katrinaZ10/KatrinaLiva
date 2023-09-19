7d22cd466cf66bc6c10fed08c5a82763efc0d2f9
katrinaZ10 <144447069+katrinaZ10@users.noreply.github.com>
2023-09-19 09:58:39 +0300

    Add files via upload

---
+++ matematika5_liva_evelinaMarta.py
@@ -0,0 +1,9 @@
+malaA = int(input('Ievadiet malas A garumu:')) 
+print(malaA)
+malaB = int(input('Ievadiet B malas garumu:'))
+print( malaB)
+high = int(input('Ievadiet augstumu:'))
+print(high)
+
+triFormula = (malaA*high) /2
+print('Laukums trijstūrim :',triFormula)
